f02df1e601e81031d1adb40cb1f6e1a278e8349c
Mahoke <mark_hoekstra1995@hotmail.com>
2018-05-25 15:10:47 +0200

    Alert, login and UI +setup alert service +setup login +setup material UI components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import 'hammerjs';
+import { LoginComponent } from './login/login.component';
+import { AlertComponent } from './_directives/alert/alert.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
